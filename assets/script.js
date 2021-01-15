51ebfd5d5b9b3951ca864532ba26f86154c7c1a7
Mark Virostek <markv86@hotmail.com>
2021-01-14 21:33:15 -0500

    turn in commit

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,33 @@
-var 
+// Variables
+let city = cleveland;
+let searchBtn = $(".btn") 
+let input = 
+let cityName = cleveland
+
+//API key
+var APIKey = "5aaef105d6f2d697fbe06f4fa073111b";
+var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + " &appid=5aaef105d6f2d697fbe06f4fa073111b";
+
+$.ajax({
+    url: "api.openweathermap.org/data/2.5/weather?q=cleveland&appid=5aaef105d6f2d697fbe06f4fa073111b",
+    get: "GET"
+}).then(function(response){
+    console.log(response)
+});
+
+// function searchCity(cityName) {
+//     $.ajax({
+//         url: queryURL,
+//         method: 'GET'
+//         console.log(cityName)
+//     })
+    // }).then(function (response) {
+    //     console.log(response);
+    //     console.log(queryURL);
+
+// function currentWeather(city){
+//     var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+//     $.ajax({
+//         url:queryURL,
+//         method:"GET",
+//         }).then(function(response)
